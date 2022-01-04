dfe19fc153521d697473941ffdebb8492e318e1f
Alexander Bychyk <sanya55bychyk@gmail.com>
2022-01-05 01:42:10 +0300

    feat: implement function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let strN = Math.abs(n).toString();
+  let newN = "";
+  for (let i = strN.length-1; i >= 0; i--) {
+    newN += strN[i];
+  } 
+  return newN;
 }
